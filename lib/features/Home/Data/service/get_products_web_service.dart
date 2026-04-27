1e2ccdad0415d98b7ab29a2312b570a78d1772d5
ziadmagdy <ziadmagdy718@gmail.com>
2026-04-27 18:40:07 +0300

    Add get_products_web_service

---
+++ lib/features/Home/Data/service/get_products_web_service.dart
@@ -0,0 +1,21 @@
+import 'package:dio/dio.dart';
+import 'package:ecommerce_app/core/constants/app_strings.dart';
+
+class GetProductsWebService {
+  late Dio dio;
+  GetProductsWebService() {
+    BaseOptions options = BaseOptions(
+      baseUrl: AppStrings.baseUrl,
+      receiveDataWhenStatusError: true,
+      receiveTimeout: Duration(seconds: 5),
+      connectTimeout: Duration(seconds: 5),
+      sendTimeout: Duration(seconds: 5),
+    );
+    dio = Dio(options);
+  }
+
+  Future<List<dynamic>> getProductsByCategory(String category) async {
+    final allCategory = await dio.get("products/category/$category");
+    return allCategory.data["products"];
+  }
+}
