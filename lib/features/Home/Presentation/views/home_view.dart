eb2ef6dafc502fe3d094c84345d7de5e99e4d051
ziadmagdy <ziadmagdy718@gmail.com>
2026-04-27 20:54:40 +0300

    Add home view

--- lib/features/Home/Presentation/views/home_view.dart
+++ lib/features/Home/Presentation/views/home_view.dart
@@ -1,13 +1,59 @@
+import 'package:ecommerce_app/features/Home/Presentation/providers/products_provider.dart';
+import 'package:ecommerce_app/features/Home/Presentation/widgets/build_list_categories.dart';
+import 'package:ecommerce_app/features/Home/Presentation/widgets/build_widget_internet_error.dart';
+import 'package:ecommerce_app/features/Home/Presentation/widgets/build_widget_is_empty_data.dart';
+import 'package:ecommerce_app/features/Home/Presentation/widgets/product_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends ConsumerWidget {
   const HomeView({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final category = ref.watch(selectedCategoryProvider);
+    final allProductsByCategory = ref.watch(productsProvider(category!));
     return Scaffold(
-      appBar: AppBar(title: const Text('Home View')),
-      body: const Center(child: Text('Welcome to the Home View!')),
+      body: Column(
+        children: [
+          SizedBox(height: 25.h),
+          Flexible(child: BuildListCategories()),
+          Row(
+            children: [
+              SizedBox(width: 10.w),
+              Text('This is $category', style: TextStyle(fontSize: 20.sp)),
+            ],
+          ),
+          Flexible(
+            flex: 4,
+            child: allProductsByCategory.when(
+              data: (data) {
+                if (data.isEmpty) {
+                  return BuildWidgetIsEmptyData();
+                }
+                return GridView.builder(
+                  padding: EdgeInsets.zero,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 2 / 3,
+                    mainAxisSpacing: 1,
+                    crossAxisSpacing: 1,
+                  ),
+                  shrinkWrap: true,
+                  physics: const ClampingScrollPhysics(),
+                  itemCount: data.length,
+                  itemBuilder: (context, index) {
+                    return ProductItem(product: data[index]);
+                  },
+                );
+              },
+              error: (error, stackTrace) => BuildWidgetInternetError(),
+              loading: () => Center(child: CircularProgressIndicator()),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
