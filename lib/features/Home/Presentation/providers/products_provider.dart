a8d6269aa3b5662eefff1df3c12f6dc919c807f7
ziadmagdy <ziadmagdy718@gmail.com>
2026-04-27 18:42:20 +0300

    Add products_provider

---
+++ lib/features/Home/Presentation/providers/products_provider.dart
@@ -0,0 +1,23 @@
+import 'package:ecommerce_app/core/constants/app_strings.dart';
+import 'package:ecommerce_app/features/Home/Data/model/product.dart';
+import 'package:ecommerce_app/features/Home/Data/repo/get_products_repository.dart';
+import 'package:ecommerce_app/features/Home/Data/service/get_products_web_service.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
+
+final productsRepoProvider = Provider<GetProductsRepository>(
+  (ref) =>
+      GetProductsRepository(getProductsWebService: GetProductsWebService()),
+);
+
+final selectedCategoryProvider = StateProvider<String?>(
+  (ref) => AppStrings.categories.first,
+);
+
+final productsProvider = FutureProvider.family<List<Product>, String>((
+  ref,
+  category,
+) async {
+  final repo = ref.read(productsRepoProvider);
+  return repo.getProductsByCategory(category);
+});
