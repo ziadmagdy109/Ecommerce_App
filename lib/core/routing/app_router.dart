9d01fc8d20fc1fc409273ac426339b9b8805849b
ziadmagdy <ziadmagdy718@gmail.com>
2026-05-01 11:57:39 +0300

    Rename some files

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,7 +1,7 @@
 import 'package:ecommerce_app/core/routing/app_routes.dart';
 import 'package:ecommerce_app/features/Home/Data/model/product.dart';
-import 'package:ecommerce_app/features/Home/Presentation/views/details_view.dart';
-import 'package:ecommerce_app/features/Home/Presentation/views/home_view.dart';
+import 'package:ecommerce_app/features/home/presentation/views/details_view.dart';
+import 'package:ecommerce_app/features/home/presentation/views/home_view.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -11,7 +11,7 @@ class AppRouter {
       GoRoute(
         path: AppRoutes.homeView,
         builder: (BuildContext context, GoRouterState state) {
-          return HomeView();
+          return const HomeView();
         },
       ),
       GoRoute(
